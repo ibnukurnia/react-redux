94242f1b8552db2dbcb01ffc27dd906a241a4ac2
ibnukurnia <ibnubudi16@gmail.com>
2022-02-09 13:56:39 +0700

    init commit

---
+++ src/reducer/index.js
@@ -0,0 +1,9 @@
+import { combineReducers } from "redux";
+
+import counterReducer from "./counterReducer";
+
+const reducer = combineReducers({
+  counter: counterReducer,
+});
+
+export default reducer;
